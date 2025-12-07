97cc34a0ee836673b88cb1eadda20d182a50bc44
piyush0310 <piyushkumar8256@gmail.com>
2025-12-07 20:51:09 +0530

    GA4_added

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,11 +8,30 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* ✅ GOOGLE SITE VERIFICATION */}
         <meta
           name="google-site-verification"
           content="NrE50hHl6c-azA8AW6DpgA0bwM54EcK2iL3JOBRgX4Q"
         />
+
+        {/* ✅ GOOGLE ANALYTICS (GA4) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-1HZF570XML"
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-1HZF570XML');
+            `,
+          }}
+        ></script>
       </head>
+
       <body className="bg-[#FFFAFA]">
         <Topbar />
         <Navbar />
